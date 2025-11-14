a017f3c2b9617b552aa59bc9a8af0c308cdc8b95
alonsoF100 <yanul228@mail.ru>
2025-11-15 00:18:12 +0300

    add gracefull shotdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
 	"log"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/alonsoF100/shotlink/internal/config"
 	"github.com/alonsoF100/shotlink/internal/logger"
+	"github.com/alonsoF100/shotlink/internal/repository/storage/postgres"
 	"github.com/alonsoF100/shotlink/internal/service"
 	"github.com/alonsoF100/shotlink/internal/transport/http/routing"
 )
@@ -17,8 +23,33 @@ func main() {
 
 	logger.Setup(cfg.Log)
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	pool, err := postgres.NewPool(ctx, cfg.Database)
+	if err != nil {
+		slog.Error("Failed to connect to database", "error", err)
+		return
+	}
+	defer pool.Close()
+
 	service := service.New(nil, nil)
 
 	router := routing.SetupRouter(service, cfg.Server.BaseURL)
-	router.Run(cfg.Server.Addr())
+
+	go func() {
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+		<-sigChan
+
+		slog.Info("Shutting down gracefully...")
+		cancel()
+		pool.Close()
+		os.Exit(0)
+	}()
+
+	slog.Info("Starting server", "port", cfg.Server.Port)
+	if err := router.Run(cfg.Server.Addr()); err != nil {
+		slog.Error("Server failed", "error", err)
+	}
 }
